e22fab8e0fab3ea5674b127f67478b366f94b683
Juan Agudelo <agudelojuanpablo398@gmail.com>
2024-05-06 12:15:16 -0500

    Introduction new issue about prototypes

---
+++ assets/index.js
@@ -0,0 +1,64 @@
+/**POO 
+ * Clases - Modelo ah seguir
+ * - Objetos- Son instancias de las Clases
+ * 
+ * - Atributos - Es una caracteristica o propiedad del Objeto
+ * (son variables dentro de un objeto)
+ * 
+ * - Metodos - Son caracteristicas que un objeto puede 
+ * realizar (son funciones dentro de un objeto)
+ */
+
+// const animal = {
+//   name: "Terranosaurio",
+//   sonar(){
+//     console.log("Hago sonidos raros")
+//   }
+// }
+
+// const animal2 = {
+//   name: "Cerdo",
+//   sonar() {
+//     console.log("Hago sonidos raros")
+//   }
+// }
+
+// // Funcion constructora 
+// function Animal(name,genre){
+//   // Atributos 
+//   this.name = name;
+//   this.genre = genre;
+// }
+// // Metodos
+// this.sonar = function () {
+//   console.log("I make sounds because i are living")
+// }
+// this.saludar = function () {
+//   console.log(`Hola me llamo ${this.name}`)
+// }
+
+
+
+// Funcion constructora donde asignamos los metodos al prototipo, 
+// no ah la funcion como tal 
+function Animal(name, genre) {
+  // Atributos 
+  this.name = name;
+  this.genre = genre;
+}
+// Metodos agregados al prototipo de la funcion constructora
+Animal.prototype.sonar = function () {
+  console.log("I make sounds because are living")
+}
+Animal.prototype.saludar = function () {
+  console.log(`Hola me llamo ${this.name}`)
+}
+const snoopy = new Animal("Peppa","Hembra")
+const Jorch = new Animal("Jorch","Macho")
+console.log(snoopy)
+console.log(Jorch)
+
+snoopy.sonar();
+snoopy.saludar();
+Jorch.sonar();
+Jorch.saludar();
